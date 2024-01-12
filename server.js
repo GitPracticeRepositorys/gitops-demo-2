eb55ce57114499ebab4f251e6724eafb72390b47
GitPracticeRepositorys <96053405+GitPracticeRepositorys@users.noreply.github.com>
2024-01-12 20:08:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Beautiful\n');
+  res.send('Hello World Beautiful Colourful\n');
 });
 
 app.listen(PORT, HOST);
